f14f566b3f71fabf3b6c352455ead97179e1c1fc
xuxiaowei088 <1262151005@qq.com>
2023-03-21 11:33:54 +0800

    create a ros init workspace

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/xuxiaowei/workspace_ros/ROS_study_workSpace/ROS_robot_first/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/xuxiaowei/workspace_ros/ROS_study_workSpace/ROS_robot_first/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/xuxiaowei/workspace_ros/ROS_study_workSpace/ROS_robot_first/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/xuxiaowei/workspace_ros/ROS_study_workSpace/ROS_robot_first/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/xuxiaowei/workspace_ros/ROS_study_workSpace/ROS_robot_first/src:$ROS_PACKAGE_PATH"
